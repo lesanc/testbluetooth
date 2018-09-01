3f286a79eca86191b1e2ceb896c0fe2560463c64
lesanc <lesanc@126.com>
2018-09-01 09:09:09 +0800

    modify connect

--- app/src/main/java/com/example/lixk/testbluetooth/MainActivity.java
+++ app/src/main/java/com/example/lixk/testbluetooth/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lixk.testbluetooth;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
@@ -105,32 +109,126 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.d(TAG, "scan: ");
         mTvConnectState.setText(getString(R.string.connect_state2));
 
-        scanner.startScan(new ScanCallback() {
-            @Override
-            public void onScanResult(int callbackType, ScanResult result) {
-                super.onScanResult(callbackType, result);
-
-                BluetoothDevice device = result.getDevice();
-
-                if (device == null || device.getName() == null){
-                    return;
-                }
-
-                if (!mDeviceList.contains(device)){
-                    mDeviceList.add(device);
-                    DeviceAdapter adapter = new DeviceAdapter(MainActivity.this, mDeviceList);
-                    adapter.setOnItemClickListener(new DeviceAdapter.OnItemClickListener() {
-                        @Override
-                        public void onClick(View view, BluetoothDevice device) {
-                            Log.d(TAG, "onClick: " + device.getName());
-                        }
-                    });
-                    mRecyclerView.setAdapter(adapter);
-                }
-                Log.d(TAG, "onScanResult: " + callbackType + " " + mDeviceList.size());
-            }
-            
-            
-        });
+        scanner.startScan(mScanCallback);
     }
+
+    @SuppressLint("NewApi")
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+
+            BluetoothDevice device = result.getDevice();
+
+            if (device == null || device.getName() == null){
+                return;
+            }
+
+            if (!mDeviceList.contains(device)){
+                mDeviceList.add(device);
+                DeviceAdapter adapter = new DeviceAdapter(MainActivity.this, mDeviceList);
+                adapter.setOnItemClickListener(new DeviceAdapter.OnItemClickListener() {
+                    @Override
+                    public void onClick(View view, BluetoothDevice device) {
+                        Log.d(TAG, "onClick: " + device.getName());
+                        mTvConnectState.setText(getString(R.string.connect_state3));
+                        BluetoothLeScanner scanner = mBluetoothAdapter.getBluetoothLeScanner();
+                        scanner.stopScan(mScanCallback);
+                        device.connectGatt(MainActivity.this, false, mBluetoothGattCallback);
+                    }
+                });
+                mRecyclerView.setAdapter(adapter);
+            }
+            Log.d(TAG, "onScanResult: " + callbackType + " " + mDeviceList.size());
+        }
+
+
+    };
+
+    private BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onPhyUpdate(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
+            super.onPhyUpdate(gatt, txPhy, rxPhy, status);
+        }
+
+        @Override
+        public void onPhyRead(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
+            super.onPhyRead(gatt, txPhy, rxPhy, status);
+        }
+
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, final int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+
+            Log.d(TAG, "onConnectionStateChange: status " + status + ", newState " + newState);
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    switch (newState){
+                        case BluetoothGatt.STATE_CONNECTING:
+                            mTvConnectState.setText(getString(R.string.connect_state4));
+                            break;
+
+                        case BluetoothGatt.STATE_CONNECTED:
+                            mTvConnectState.setText(getString(R.string.connect_state5));
+                            break;
+
+                        case BluetoothGatt.STATE_DISCONNECTING:
+                            mTvConnectState.setText(getString(R.string.connect_state6));
+                            break;
+
+                        case BluetoothGatt.STATE_DISCONNECTED:
+                            mTvConnectState.setText(getString(R.string.connect_state7));
+                            break;
+                    }
+                }
+            });
+
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+        }
+
+        @Override
+        public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorRead(gatt, descriptor, status);
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorWrite(gatt, descriptor, status);
+        }
+
+        @Override
+        public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
+            super.onReliableWriteCompleted(gatt, status);
+        }
+
+        @Override
+        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+            super.onReadRemoteRssi(gatt, rssi, status);
+        }
+
+        @Override
+        public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
+            super.onMtuChanged(gatt, mtu, status);
+        }
+    };
 }
